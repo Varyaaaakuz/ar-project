12eea0ae221e8e99b0d4d4f3cd18d48b285c7e7f
Varyaaaakuz <variakuzmina3003@gmail.com>
2023-12-19 03:08:38 +0300

    new

---
+++ Assets/scripts/PlayerMoney.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoney : MonoBehaviour
+{
+    [SerializeField] public int _moneyAmount;
+    public void ProcessWin(int points)
+    {
+        _moneyAmount += points;
+    }
+
+    public void ProcessBuy(int money)
+    {
+
+        if (_moneyAmount - money < 0)
+        {
+            return;
+        }
+
+        _moneyAmount-= money;
+    }
+
+    public bool CanBuy(int price)
+    {
+        return _moneyAmount - price >= 0;
+    }
+}
