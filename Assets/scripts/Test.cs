5774497b44aec84485a3b038617431d95064a23e
user <variakuzmina3003@gmail.com>
2023-12-28 15:17:42 +0300

    konch

--- Assets/scripts/Test.cs
+++ Assets/scripts/Test.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        var _position = Camera.main.transform.position + Camera.main.transform.forward * 1.7f;
+        var _position = Camera.main.transform.position + Camera.main.transform.forward * 3.5f;
         this.transform.position = new Vector3(_position.x, this.transform.position.y, _position.z);
     }
 }
